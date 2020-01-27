e28ffc6620c14ad6c805ac676b5b99fa79acad92
Rakshana-raksh146 <59764030+Rakshana-raksh146@users.noreply.github.com>
2020-01-27 20:54:05 +0530

    Delete Result.js

--- Result.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Result = ({score,playAgain}) => (
-	<div className="score-board">
-		<div className="score">You have Scored {score} / 5 correct answers!</div>
-		<button className="playBtn" onClick={playAgain}>Play Again!</button> 
-	</div>
-);
-
-export default Result;
